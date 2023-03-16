6ffe10000a899cc32c67acd889a03edc06d5e14d
Alitor004 <irmaosgomes1302@gmail.com>
2023-03-15 21:32:02 -0300

    Criacao do dado

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById<Button>(R.id.Roll)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Rolou o dado!", Toast.LENGTH_SHORT).show()
+            rollDice()
+          }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll (): Int {
+        return (1..numSides).random()
+    }
+}
